ed1936e1bbd23a3fd7618a30592088628b7bc2cf
Ian Hilt <ian@sc-techservices.com>
2019-08-16 23:23:07 -0400

    Style updates

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="absolute inset-0 flex justify-center items-center bg-gray-200">
     <main class="container m-4">
-      <section>
-        <h1 class="text-2xl text-gray-700">Hi!</h1>
-        <p class="font-mono">I'm Ian Hilt and I'm a full stack developer.</p>
-        <p class="font-mono">Stay tuned as I'll be updating this site in the near future.</p>
-        <p class="font-mono">In the meantime, you can get in touch <a class="underline" href="https://sc-techservices.com/contact-us/" target="_blank">here</a>.</p>
+      <section class="flex flex-col bg-gray-100 md:flex-row shadow-lg p-4 md:p-6 lg:p-8">
+        <div class="rounded-lg overflow-hidden my-4 md:my-0 md:mr-6 lg:mr-8 self-center">
+          <a href="https://github.com/ihilt"><img class="w-32" alt="Avatar" src="https://avatars1.githubusercontent.com/u/279639"></a>
+        </div>
+        <div>
+          <h1 class="font-sans text-3xl text-gray-700">Hi!</h1>
+          <p class="font-mono">I'm Ian Hilt and I'm a full stack developer.</p>
+          <p class="font-mono">Stay tuned as I'll be updating this site in the near future.</p>
+          <p class="font-mono">In the meantime, you can get in touch <a class="underline" href="https://sc-techservices.com/contact-us/">here</a>
+          or <a class="underline" href="https://twitter.com/ianhilt">here</a>.</p>
+        </div>
       </section>
     </main>
   </div>
